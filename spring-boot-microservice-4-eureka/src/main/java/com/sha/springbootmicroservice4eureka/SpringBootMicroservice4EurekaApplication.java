e58b55c750e6b4aaa9f9d9d781659cd327ad2b6b
Praveen Kumar C <81486277+praveenc9397@users.noreply.github.com>
2023-05-26 02:33:25 +0530

    Update SpringBootMicroservice4EurekaApplication.java

--- spring-boot-microservice-4-eureka/src/main/java/com/sha/springbootmicroservice4eureka/SpringBootMicroservice4EurekaApplication.java
+++ spring-boot-microservice-4-eureka/src/main/java/com/sha/springbootmicroservice4eureka/SpringBootMicroservice4EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.sha.springbootmicroservice4eureka;
+package com.pkc.springbootmicroservice4eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
